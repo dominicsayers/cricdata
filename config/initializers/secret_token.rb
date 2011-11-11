01691e63d6e8b2e315e08b791212e19f09bdc970
Dominic Sayers <dominic@sayers.cc>
2011-11-11 18:57:22 +0000

    Raw HTML5 boilerplate app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cricdata::Application.config.secret_token = 'e16fad6aec97b5829d9f50b875b7ff7707087e8bae608445b9680d2c0f65e9dd9833430913339e4dc2a74c278f33dd687938ff3e1a9d085b93a7c9fe3c300c0c'
